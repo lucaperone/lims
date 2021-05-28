97aadbcb3dd4472e09703cf8599b040b39bfc397
Luca Perone <luca.perone@jooce.ch>
2021-05-28 14:49:43 +0200

    Fetch Data V2 with error handling

--- fetch_data.js
+++ fetch_data.js
@@ -8,42 +8,80 @@ const libraries_url =
 
 async function fetchData() {
 	console.log("Starting applcation...\n")
+	const { libraries, requests } = await webScrapper()
+
+	console.log("Merging requests...")
+
+	const pools = new Array()
+
+	for (let i = 0; i < requests.length; i++) {
+		multiplex = parseInt(requests[i][7])
+		if (multiplex === NaN) {
+			return {
+				error: `Error with request ${requests[i][0]} - Multiplex# "${requests[i][7]}" is not a number`,
+			}
+		}
+
+		const pool = parseAndMergeRequests(
+			requests.slice(i, i + multiplex),
+			multiplex
+		)
+
+		if (pool.error != "") {
+			return { error: pool.error }
+		} else {
+			pool.ready = isReady(pool, libraries)
+			pools.push(pool)
+		}
+		i += multiplex - 1
+	}
+
+	console.log("Merged requests into " + pools.length + " pools\n")
+
+	return {
+		error: "",
+		pools: pools,
+	}
+}
+
+async function webScrapper() {
+	results = {
+		libraries: [],
+		requests: [],
+	}
+
 	const browser = await puppeteer.launch()
-	const login_page = await browser.newPage()
-	await login_page.goto(login_url)
+	const page = await browser.newPage()
+	await page.goto(login_url)
 
 	// Login
-	await login_page.type("#lg_name", "lperone")
-	await login_page.type("#lg_password", "tkk4dbvb")
-	await login_page.click("input[type='submit']")
+	await page.type("#lg_name", "lperone")
+	await page.type("#lg_password", "tkk4dbvb")
+	await page.click("input[type='submit']")
 
 	// Get cookies
-	const cookies = await login_page.cookies()
+	const cookies = await page.cookies()
 
 	console.log("Fetching libraries...")
 
 	// Use cookies in another tab or browser
-	const libraries_page = await browser.newPage()
-	await libraries_page.setCookie(...cookies)
+	const new_page = await browser.newPage()
+	await new_page.setCookie(...cookies)
 	// Open the page as a logged-in user
-	await libraries_page.goto(libraries_url)
+	await new_page.goto(libraries_url)
 
-	const libraries = await libraries_page.$$eval(
+	results.libraries = await new_page.$$eval(
 		"table.sortable tbody tr td:nth-child(2)",
 		(names) => {
 			return Array.from(names, (name) => name.innerText)
 		}
 	)
-	console.log("Found " + libraries.length + " libraries\n")
+	console.log("Found " + results.libraries.length + " libraries\n")
 	console.log("Fetching requests...")
 
-	// Use cookies in another tab or browser
-	const requests_page = await browser.newPage()
-	await requests_page.setCookie(...cookies)
-	// Open the page as a logged-in user
-	await requests_page.goto(requests_url)
+	await new_page.goto(requests_url)
 
-	const requests = await requests_page.$$eval(
+	results.requests = await new_page.$$eval(
 		"table.sortable tbody tr",
 		(rows) => {
 			return Array.from(rows, (row) => {
@@ -52,96 +90,82 @@ async function fetchData() {
 			})
 		}
 	)
+
+	console.log("Found " + results.requests.length + " requests\n")
+
 	await browser.close()
 
-	console.log("Found " + requests.length + " requests\n")
+	return results
+}
 
-	console.log("Merging requests...")
-
-	var merged_requests = new Array()
-	var groups_indexed = new Array()
-	var nb_untitled = addRequest(
-		requests[0],
-		merged_requests,
-		0,
-		groups_indexed
-	)
-
-	for (let i = 0; i < requests.length - 1; i++) {
-		const row = requests[i]
-		const next_row = requests[i + 1]
-
-		if (
-			row[2] == next_row[2] && // Same Laboratory
-			row[3] == next_row[3] && // Same Library
-			row[4] == next_row[4] && // Same Run type
-			row[5] == next_row[5] && // Same Read Length
-			row[8] == next_row[8] && // Same Group
-			row[9] == next_row[9] // Same Submitter
-		) {
-			if (row[8] == "") {
-				updateRequest(
-					next_row,
-					merged_requests,
-					groups_indexed.indexOf("untitled" + nb_untitled)
-				) // Similar but untitled
-			} else {
-				updateRequest(
-					next_row,
-					merged_requests,
-					groups_indexed.indexOf(row[8])
-				) // Similar and titled
+function parseAndMergeRequests(requests, multiplex) {
+	if (requests.every((request) => similar(request, requests))) {
+		const run_type = parseRun(requests[0][4])
+		if (run_type === "error") {
+			return {
+				error: `Error with request ${requests[0][0]} - Unkown run type "${requests[0][4]}"`,
 			}
 		} else {
-			if (groups_indexed.includes(next_row[8]) && next_row != "") {
-				updateRequest(
-					next_row,
-					merged_requests,
-					groups_indexed.indexOf(next_row[8])
-				) // Not similar, titled, already present
-			} else {
-				nb_untitled = addRequest(
-					next_row,
-					merged_requests,
-					nb_untitled,
-					groups_indexed
-				) // New
+			const lanes = parseFloat(requests[0][6])
+			if (lanes === NaN) {
+				return {
+					error: `Error with request ${requests[0][0]} - Nb lanes "${requests[0][6]}" is not a number`,
+				}
+			}
+			return {
+				error: "",
+				libraries: groupLibraries(requests),
+				lab: requests[0][2],
+				protocol: requests[0][3],
+				run: run_type,
+				read: requests[0][5],
+				lanes: lanes,
+				multiplex: multiplex,
+				group: requests[0][8],
+				submitter: requests[0][9],
+				ready: false,
 			}
 		}
+	} else {
+		return {
+			error: `Error with request ${requests[0][0]} - Number of multiplex given: ${requests[0][7]}, not enough similar requests found.`,
+		}
 	}
-	console.log("Merged requests into " + merged_requests.length + " pools\n")
-	console.log("Filtering pools...")
+}
 
-	filtered_requests = merged_requests.filter((request) =>
-		request[1].every((library) => libraries.includes(library))
-	)
-	console.log(filtered_requests.length + " pools after filtering\n")
+function similar(request, requests) {
+	let similar = true
+	for (let i = 2; i < 10; i++) {
+		similar &= request[i] === requests[0][i]
+	}
+	return similar
+}
 
-	return filtered_requests
+function groupLibraries(requests) {
+	const libraries = new Array()
+	requests.forEach((request) => {
+		libraries.push(request[1])
+	})
+	return libraries
+}
+
+function parseRun(run) {
+	switch (run) {
+		case "single read":
+			return "SR"
+		case "paired-end reads":
+			return "PE"
+		default:
+			return "error"
+	}
+}
+
+function isReady(pool, libraries) {
+	return pool.libraries.every((library) => libraries.includes(library))
 }
 
 function cleanupRow(row) {
 	return [row[8], [], row[2], row[9], row[3], row[4], row[5], [], []]
-}
-
-function addRequest(row, requests, nb_untitled, groups_indexed) {
-	var cleaned_row = cleanupRow(row)
-	if (row[8] == "") {
-		nb_untitled++
-		cleaned_row[0] = "untitled" + nb_untitled
-	} else {
-		cleaned_row[0] = row[8]
-	}
-	groups_indexed.push(cleaned_row[0])
-	requests.push(cleaned_row)
-
-	return nb_untitled
-}
-
-function updateRequest(row, requests, index) {
-	requests[index][1].push(row[1]) // Store all libraries
-	requests[index][7].push(row[6]) // Store all lanes (should be 1)
-	requests[index][8].push(row[7]) // Store all Multiplex (should be 1)
 }
 
 module.exports = {
@@ -149,3 +173,9 @@ module.exports = {
 		return await fetchData()
 	},
 }
+
+// async function main() {
+// 	console.log(await fetchData())
+// }
+
+// main()
