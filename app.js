e7fdfcda09dde48f6bb87e092f8cc355afcd90a9
Luca Perone <luca.perone@jooce.ch>
2021-07-01 21:42:15 +0200

    Better app.js

--- app.js
+++ app.js
@@ -11,17 +11,11 @@ app.use(logger("dev"))
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")))
 
-// Route for everything else.
-app.get("/", function (req, res) {
-	res.send("Hello World")
-})
-
 // Fetch data route
 app.get("/data", async (req, res) => {
 	const data = await dataFetcher.fetchData()
 	res.send(data)
 })
 
-// Fire it up!
 app.listen(3000)
 console.log("Listening on port 3000")
